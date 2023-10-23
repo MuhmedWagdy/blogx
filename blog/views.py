d88b930e6ea8818b8059f8fe4f5ade4673bd88be
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-23 12:13:31 +0300

    add urls id: int :

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+from .models import Post
+
+
+def post_list(request):
+
+    data = Post.objects.all()
+
+    return render(request,'post_list.html',{'post':data})
+
+
+def post_detail(request,post_id):
+
+      data = Post.objects.get(id=post_id)
+
+      return render(request,'post_detail.html',{'post':data})
