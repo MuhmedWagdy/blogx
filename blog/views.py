fd501118f6e6bcc99ab03c5ee13f9c357747f3ec
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-23 15:52:34 +0300

    add crud operations

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import PostForm
 
 # Create your views here.
 
@@ -18,3 +19,44 @@ def post_detail(request,post_id):
       data = Post.objects.get(id=post_id)
 
       return render(request,'post_detail.html',{'post':data})
+
+
+
+def post_new(request):
+    if request.method == 'POST':         
+          form = PostForm(request.POST,request.FILES)
+          if form.is_valid():
+              myform= form.save(commit=False)
+              myform.author = request.user
+              myform.save()
+              return redirect('/blog')    
+    else:
+        form = PostForm()
+    return render(request,'new_post.html',{'form':form})
+
+
+def edit_post(request,post_id):
+    data = Post.objects.get(id=post_id)
+    if request.method == 'POST':         
+          form = PostForm(request.POST,request.FILES,instance=data)
+          if form.is_valid():
+              myform= form.save(commit=False)
+              myform.author = request.user
+              myform.save()
+              return redirect('/blog')        
+    else:
+        form = PostForm(instance=data)
+    return render(request,'edit_post.html',{'form':form})
+
+     
+
+
+
+def delete_post(request,post_id):
+     data = Post.objects.get(id=post_id)
+     data.delete()
+     return redirect('/blog')
+     
+
+
+
--- project/urls.py
+++ project/urls.py
@@ -19,12 +19,17 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from blog.views import post_list,post_detail
+from blog.views import post_list,post_detail,post_new,edit_post,delete_post
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('blog/', post_list),
+    path('blog/new', post_new),
     path('blog/<int:post_id>', post_detail),
+    path('blog/<int:post_id>/edit', edit_post),
+    path('blog/<int:post_id>/delete', delete_post),
+    
 
 ]
 
