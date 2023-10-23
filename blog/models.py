86a12d7b610f830bb6d6b66bb781744056f37602
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-23 10:35:33 +0300

    add tagit lib

--- blog/models.py
+++ blog/models.py
@@ -1,8 +1,11 @@
 from django.db import models
-
 from django.utils import timezone
-
+from taggit.managers import TaggableManager
+from django.contrib.auth.models import User      
 # Create your models here.
+
+
+
 
 class Post(models.Model):
 
@@ -10,3 +13,12 @@ class Post(models.Model):
     content = models.TextField(max_length=3000)
     created_date = models.DateTimeField(default=timezone.now)
     draft = models.BooleanField(default=True)
+    tags = TaggableManager()
+
+    author = models.ForeignKey(User,on_delete=models.SET_NULL,null= True,blank=True,related_name='post_user')
+
+
+
+
+    def __str__(self):
+        return self.title
