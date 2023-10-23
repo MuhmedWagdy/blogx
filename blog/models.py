32897c79c7cc0fe9d10e6e0ea5ee3790c5d7bd2a
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-23 09:20:03 +0300

    add new table post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+from django.utils import timezone
+
 # Create your models here.
+
+class Post(models.Model):
+
+    title = models.CharField(max_length=100)
+    content = models.TextField(max_length=3000)
+    created_date = models.DateTimeField(default=timezone.now)
+    draft = models.BooleanField(default=True)
