e119a1a991ed366102a0f3a2066f0ad4ff35e2ba
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-24 16:19:36 +0300

    add Class Based Views

--- blog/views.py
+++ blog/views.py
@@ -3,15 +3,33 @@ from .forms import PostForm
 # Create your views here.
 from .models import Post
 
+from django.views.generic import ListView,DetailView,CreateView,UpdateView,DeleteView
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+  
+
 
 def post_list(request):
     data = Post.objects.all()
     return render(request,'post_list.html',{'post':data})
 
 
+class PostList(ListView):
+     model = Post
+# ===============================================================================================================================================================
+
 def post_detail(request,post_id):
       data = Post.objects.get(id=post_id)
       return render(request,'post_detail.html',{'post':data})
+
+
+class PostDetail(DetailView):
+     model = Post
+     
+
+#===================================================================================================================================================================
+
 
 
 
@@ -28,6 +46,15 @@ def post_new(request):
     return render(request,'new_post.html',{'form':form})
 
 
+class PostCreate(CreateView):
+     model = Post
+     fields = ['title','content','created_date','draft','tags','author','image']
+
+     success_url = '/blog'
+
+
+
+#===========================================================================================================================================================
 def edit_post(request,post_id):
     data = Post.objects.get(id=post_id)
     if request.method == 'POST':         
@@ -41,14 +68,27 @@ def edit_post(request,post_id):
         form = PostForm(instance=data)
     return render(request,'edit_post.html',{'form':form})
 
-     
 
 
+
+class PostUpdate(UpdateView):
+     model = Post
+     fields = ['title','content','created_date','draft','tags','author','image']
+     success_url = '/blog'
+
+     template_name='blog/edit_post.html'
+#============================================================================================================================================================================
 
 def delete_post(request,post_id):
      data = Post.objects.get(id=post_id)
      data.delete()
      return redirect('/blog')
+
+
+class DeletePost(DeleteView):
+     model = Post
+     success_url = '/blog'
+
      
 
 
