df761aebbf521ae938043e91de1083d04bb0ad76
muhmedwagdy <mohamed.wagdy2016@gmail.com>
2023-10-24 14:04:12 +0300

    add class based views

--- blog/models.py
+++ blog/models.py
@@ -14,9 +14,7 @@ class Post(models.Model):
     created_date = models.DateTimeField(default=timezone.now)
     draft = models.BooleanField(default=True)
     tags = TaggableManager()
-
     author = models.ForeignKey(User,on_delete=models.SET_NULL,null= True,blank=True,related_name='post_user')
-
     image = models.ImageField(upload_to='posts')
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,16 @@
 from django.shortcuts import render,redirect
 from .forms import PostForm
-
 # Create your views here.
-
-
 from .models import Post
 
 
 def post_list(request):
-
     data = Post.objects.all()
-
     return render(request,'post_list.html',{'post':data})
 
 
 def post_detail(request,post_id):
-
       data = Post.objects.get(id=post_id)
-
       return render(request,'post_detail.html',{'post':data})
 
 
